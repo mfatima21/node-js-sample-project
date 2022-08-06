01c9aa35feefea77747a8aceca0524ca56256c35
Ghaxi007 <hafiz.ghazimujadad@gmail.com>
2022-08-06 02:45:54 -0700

    Adding Files

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const dotenv = require("dotenv");
 
 const index= require('./routes/index.routes');
 const { query } = require("express");
+const jwt = require('jsonwebtoken');
 
 dotenv.config();
 
@@ -45,4 +46,42 @@ app.use('/', index)
 
 app.listen(3000, () => {
   console.log("server is up and running");
+});
+//Jwt authentication
+app.post("/user/generateToken", (req, res) => {
+  // Validate User Here
+  // Then generate JWT Token
+
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+  let data = {
+      time: Date(),
+      userId: 12,
+  }
+
+  const token = jwt.sign(data, jwtSecretKey);
+
+  res.send(token);
+});
+
+// Verification through JWT
+app.get("/user/validateToken", (req, res) => {
+
+
+  let tokenHeaderKey = process.env.TOKEN_HEADER_KEY;
+  let jwtSecretKey = process.env.JWT_SECRET_KEY;
+
+  try {
+      const token = req.header(tokenHeaderKey);
+
+      const verified = jwt.verify(token, jwtSecretKey);
+      if(verified){
+          return res.send("Successfully Verified");
+      }else{
+         
+          return res.status(401).send(error);
+      }
+  } catch (error) {
+   
+      return res.status(401).send(error);
+  }
 });
